6d2bed8b5d928160d392cddf7eaeaf51224e7abb
Scott P Perry <perry.no3@me.com>
2016-09-06 19:03:21 +1000

    basic menu structure

---
+++ shopping_list.py
@@ -0,0 +1,30 @@
+print("Shopping List 1.0 - by Scott Pete Perry")
+print("csv item count" "items loaded from" "items.csv")
+print("Menu:")
+print("R - List required items")
+print("C - List completed items")
+print("M - Mark and item as completed")
+print("A - Add new items")
+print("Q - Quit")
+
+menu_choice = input("Please select and option from above")
+while menu_choice != str:
+    if menu_choice == 'r':
+        print("Required Items:")
+    elif menu_choice == 'c':
+        print("Completed Items:")
+    elif menu_choice == 'm':
+        print("list of items. new line 1, new line 2")
+    elif menu_choice == 'a':
+        print("Item name:")
+
+
+print("Input Error")
+
+
+
+
+
+
+
+
