4f7e3d9c11e4cf7e30ec40c72c7833734f5c22c3
Scott P Perry <perry.no3@me.com>
2016-09-07 18:27:54 +1000

    menu redone, added functional loop to menu.

--- shopping_list.py
+++ shopping_list.py
@@ -1,14 +1,29 @@
-print("Shopping List 1.0 - by Scott Pete Perry")
-print("csv item count" "items loaded from" "items.csv")
-print("Menu:")
-print("R - List required items")
-print("C - List completed items")
-print("M - Mark and item as completed")
-print("A - Add new items")
-print("Q - Quit")
+# Imports the CSV file into Python
+import csv
 
-menu_choice = input("Please select and option from above")
-while menu_choice != str:
+
+print("Shopping List 1.0 - by Scott Pete Perry")
+
+with open('items.csv', "r") as f:
+    reader = csv.reader(f, delimiter=",")
+    data = list(reader)
+    row_count = len(data)
+print("{} items loaded from items.csv".format(row_count))
+
+
+def print_menu():
+    print("Menu:")
+    print("R - List required items")
+    print("C - List completed items")
+    print("M - Mark and item as completed")
+    print("A - Add new items")
+    print("Q - Quit")
+    print("Please select an option from above")
+
+loop = True
+while loop:
+    print_menu()
+    menu_choice = str(input())
     if menu_choice == 'r':
         print("Required Items:")
     elif menu_choice == 'c':
@@ -17,9 +32,13 @@ while menu_choice != str:
         print("list of items. new line 1, new line 2")
     elif menu_choice == 'a':
         print("Item name:")
+    elif menu_choice == 'q':
+        loop = False
+    else:
+        input("Input Error! Enter any key to try again.")
 
+print("Program exiting")
 
-print("Input Error")
 
 
 
